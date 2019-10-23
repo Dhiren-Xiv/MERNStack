fd8f487699f60716d2d55bb1498981f20c4baddc
Dhiren-Xiv <dhiren905@gmail.com>
2019-10-23 23:39:42 +0530

    adding and deleting the education.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -190,4 +190,56 @@ router.delete('/experience/:exp_id', auth, async (req, res) => {
         res.status(500).send("Internal Server Error")
     }
 });
+
+// @rote    PUT api/profile/education
+// @desc    Add profile education
+// @access  Private
+
+router.put('/education', [auth, [
+    check('school', 'Schoool is required').not().isEmpty(),
+    check('degree', 'DegreeCompany is required').not().isEmpty(),
+    check('from', 'From date is required').not().isEmpty(),
+    check('fieldofstudy', 'Field of study is required').not().isEmpty(),
+]], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+    const { school, degree, fieldofstudy, from, to, current, description } = req.body;
+    const newEdu = {
+        school,
+        degree,
+        fieldofstudy,
+        from,
+        to,
+        current,
+        description
+    }
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+        profile.education.unshift(newEdu);
+        await profile.save()
+        res.json(profile);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error")
+    }
+})
+
+// @rote    DELET api/profile/education/:edu_id
+// @desc    Delete profile education
+// @access  Private
+router.delete('/education/:edu_id', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+        // get the removeIndex
+        const removeIndex = profile.education.map(item => item.id).indexOf(req.params.edu_id);
+        profile.education.splice(removeIndex, 1);
+        await profile.save();
+        res.json(profile);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error")
+    }
+});
 module.exports = router;
