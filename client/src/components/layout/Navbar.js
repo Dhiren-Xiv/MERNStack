8ef3db63bace78d8207fbb070b471a807b56df6f
Dhiren-Xiv <dhiren905@gmail.com>
2019-10-27 00:58:46 +0530

    Adding react router to the projects.

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,20 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = props => {
     return (
         <nav className="navbar bg-dark">
             <h1>
-                <a href="index.html"><i className="fas fa-code" /> DevConnector</a>
+                <Link to="/">
+                    <i className="fas fa-code" /> DevConnector
+                </Link>
             </h1>
             <ul>
                 <li>
-                    <a href="profiles.html">Developers</a>
+                    <Link to="profiles">
+                        Developers
+                    </Link>
                 </li>
                 <li>
-                    <a href="register.html">Register</a>
+                    <Link to="/register">
+                        Register
+                    </Link>
                 </li>
                 <li>
-                    <a href="login.html">Login</a>
+                    <Link to="/login">
+                        Login
+                    </Link>
                 </li>
             </ul>
         </nav>
