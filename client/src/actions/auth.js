728553f77ab41739f676762606daa6b90b7316e5
Dhiren-Xiv <dhiren905@gmail.com>
2019-12-10 23:31:30 +0530

    Add logout and its related navbar changes.

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -6,7 +6,8 @@ import {
     USER_LOADED,
     AUTH_ERROR,
     LOGIN_SUCCESS,
-    LOGIN_FAIL
+    LOGIN_FAIL,
+    LOGOUT
 } from '../actions/types';
 import setAuthToken from '../utils/setAuthToken';
 
@@ -85,4 +86,12 @@ export const login = (email, password) => async dispatch => {
             type: LOGIN_FAIL
         });
     }
+}
+
+//Logut clear profile
+export const logout = () => dispatch => {
+    console.log("logout called.");
+    dispatch({
+        type: LOGOUT
+    })
 }
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { logout } from '../../actions/auth';
+const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
 
-const Navbar = props => {
+    const authLinks = (
+        <ul>
+            <li>
+                <a onClick={logout} href="#!">
+                    <i className="fas fa-sign-out-alt"></i>{' '}
+                    <span className="hide-sm">Logout</span>
+                </a>
+            </li>
+        </ul>
+    );
+    const guestLinks = (
+        <ul>
+            <li>
+                <a href="#!">
+                    Developers
+                    </a>
+            </li>
+            <li>
+                <Link to="/register">
+                    Register
+                    </Link>
+            </li>
+            <li>
+                <Link to="/login">
+                    Login
+                    </Link>
+            </li>
+        </ul>
+    );
     return (
         <nav className="navbar bg-dark">
             <h1>
@@ -9,25 +41,16 @@ const Navbar = props => {
                     <i className="fas fa-code" /> DevConnector
                 </Link>
             </h1>
-            <ul>
-                <li>
-                    <Link to="profiles">
-                        Developers
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/register">
-                        Register
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/login">
-                        Login
-                    </Link>
-                </li>
-            </ul>
+            {!loading && (<Fragment>{(isAuthenticated) ? authLinks : guestLinks}</Fragment>)}
         </nav>
     )
 }
 
-export default Navbar
+Navbar.propTypes = {
+    logout: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+}
+const mapStateToProp = state => ({
+    auth: state.auth
+})
+export default connect(mapStateToProp, { logout })(Navbar)
